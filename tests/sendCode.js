7dc8649d79d4dc33444c264cf923b234ce120a1e
ikealmighty <isaacyates7@gmail.com>
2024-12-26 14:21:13 -0600

    changed sendCode test to just log hello to server console

--- tests/sendCode.js
+++ tests/sendCode.js
@@ -1,5 +1,4 @@
 const { Socket } = require("net");
-
 const client = new Socket();
 client.setEncoding("utf8");
 
@@ -9,4 +8,4 @@ client.connect(8080, "localhost", () => {
 
 client.on("data", (data) => console.log(data));
 
-client.write(`{ "entityId": "54861c87-f5ea-40d4-b658-9b09de47e4fd", "code": "dev_addSatelite('hello');" }`);
+client.write(`{ "entityId": "test", "code": "log('hello');" }`);
