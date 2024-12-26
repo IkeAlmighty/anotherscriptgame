dcf6538c02a1852f518ec26dbc6a6b4b0d483ca5
ikealmighty <isaacyates7@gmail.com>
2024-12-26 14:20:10 -0600

    edited test collection object, added dev functions permission

--- db.js
+++ db.js
@@ -1,15 +1,22 @@
 // TODO: replace with real database:
 
 const collection = {
-    '54861c87-f5ea-40d4-b658-9b09de47e4fd': {
-        owner: 'dev',
-        satelites: ['54861c87-f5ea-40d4-b658-9b09de47e4fd']
+    "54861c87-f5ea-40d4-b658-9b09de47e4fd": {
+        owner: "dev",
+        satelites: ["54861c87-f5ea-40d4-b658-9b09de47e4fd"],
     },
-    'ac2469b7-7ea6-4edc-abb3-6c20b8f72079': {
-        owner: 'ikealmighty',
-    }
-}
+    "ac2469b7-7ea6-4edc-abb3-6c20b8f72079": {
+        owner: "ikealmighty",
+    },
+    test: {
+        owner: "dev",
+        satelites: [
+            "54861c87-f5ea-40d4-b658-9b09de47e4fd",
+            "ac2469b7-7ea6-4edc-abb3-6c20b8f72079",
+        ],
+    },
+};
 
 module.exports = {
-    collection: () => collection
+    collection: () => collection,
 };
--- game-auth.js
+++ game-auth.js
@@ -6,11 +6,14 @@ function applyPermissions(owner, functions) {
     const allowedFunctions = {};
 
     for (let key in functions) {
-        if (owner !== 'dev') {
-            if (key.substring(0, 4) !== 'dev_') {
+        if (owner !== "dev") {
+            // if the owner is not a dev, then only add the function
+            // if they function is a non-dev function.
+            if (key.substring(0, 4) !== "dev_") {
                 allowedFunctions[key] = functions[key];
             }
         } else {
+            // if the owner is 'dev', then always add the function
             allowedFunctions[key] = functions[key];
         }
     }
